d6b6d420ae0dc2f5fe85b67abcd9969f391596e7
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-05-12 16:30:51 +0300

    add validate func user

--- internal/data/users.go
+++ internal/data/users.go
@@ -59,3 +59,19 @@ func ValidatePasswordPlaintext(v *validator.Validator, password string) {
 	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
 	v.Check(len(password) <= 72, "password", "must not be more that 72 bytes long")
 }
+
+func validateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must be not more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+
+}
