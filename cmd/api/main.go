3ce502288bf05373a3617ca354100058f197a9e8
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-05-18 15:12:30 +0300

    enable dynamic access-control-origin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -39,6 +40,10 @@ type config struct {
 		password string
 		sender   string
 	}
+
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -69,6 +74,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "80f4b8dfc16a40", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "699b26469a849e", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "nphilibert17@gmail.com", "SMTP password")
+
+	flag.Func("cors-trusted-origins", "trusted CORS origins(space separedted)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -187,7 +187,19 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		w.Header().Add("Vary", "Origin")
+
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for _, v := range app.config.cors.trustedOrigins {
+				if origin == v {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
 
 		next.ServeHTTP(w, r)
 	})
